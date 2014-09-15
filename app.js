eec0f3b94b1ef8dbf2850cda439cf542599a48db
Adrian Kim <uknoiluv@gmail.com>
2014-09-14 18:29:22 -0700

    9/14_googleMap_working

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var app = angular.module('mapnote', []);
+var app = angular.module('mapnote', ['google-maps']);
 
 app.controller('MapController', function($scope){
-  
-})
+  $scope.map = {
+    center: {
+      latitude: 49,
+      longitude: -73
+    },
+    zoom: 8
+  }
+});
 
 app.controller('NoteController', function($scope){
   $scope.list = [];
