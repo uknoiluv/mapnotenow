f948ea71b5fb81854c6bb568684e12a826eda503
Adrian Kim <uknoiluv@gmail.com>
2014-09-29 16:51:24 -0700

    9/29_backend_start

---
+++ requestHandler.js
@@ -0,0 +1,7 @@
+
+
+
+var handlerRequest = function(request, response){
+
+
+}
---
+++ server.js
@@ -0,0 +1,10 @@
+
+var http = require('http');
+
+var port = 3000;
+
+var ip = '127.0.0.1';
+
+var server = http.createServer(handleRequest);
+console.log('listening on http://' + ip + ':' + port);
+server.listen(port, ip);
