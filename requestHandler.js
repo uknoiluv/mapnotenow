bfd161837671a20b3a74f32414e545e0f77b137f
Adrian Kim <uknoiluv@gmail.com>
2014-09-29 23:27:04 -0700

    9/29_EISDIR_err_fix

--- requestHandler.js
+++ requestHandler.js
@@ -6,45 +6,57 @@ var exports = module.exports = {};
 
 exports.handler = function(request, response){
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  var pathName = '.'
-  
-  if(request.url === '/'){
-    pathName = './index.html';
-  }else{
-    fs.exists('.' + request.url, function(exist){
-      if(!exist){
-        response.writeHead(500);
-        response.end();
-      }else{
-        pathName = '.' + request.url;
-      }
-    });
-  }
+  var pathName = '.' + request.url;
 
-  fs.readFile(pathName, function(err, content){
-    console.log('pathName', pathName);
-    if(err){
-      throw err;
+  if(pathName === './'){
+    pathName = './index.html';
+  }
+  // else{
+    // fs.exists('.' + request.url, function(exist){
+    //   console.log('exist', exist);
+    //   if(!exist){
+    //     response.writeHead(500);
+    //     response.end();
+    //   }else{
+    //     pathName = '.' + request.url;
+    //   }
+    // });
+  // }
+
+  path.exists(pathName, function(exists){
+    if(exists){
+      fs.readFile(pathName, function(err, content){
+        console.log('pathName', pathName);
+        if(err){
+          throw err;
+        }else{
+          var pathExt = path.extname(pathName);
+          console.log('pathExt + ', pathExt, request.url);
+          var send = function(cont, headKey, headVal){
+            var header = {};
+            header[headKey] = headVal;
+            response.writeHeader(200, header);
+            response.write(cont);
+            response.end();
+          };
+          if(pathExt === '.js'){
+            send(content, 'Content-Type', 'text/javascript')
+          }else if(pathExt === '.html'){
+            send(content, 'Content-Type', 'text/html');
+          }else if(pathExt === '.css'){
+            send(content, 'Content-Type', 'text/css')
+          }else{
+            send(500);
+          }
+        }
+      });
     }else{
-      var pathExt = path.extname(pathName);
-      console.log('pathExt + ', pathExt, request.url);
-      var send = function(cont, headKey, headVal){
-        var header = {};
-        header[headKey] = headVal;
-        response.writeHeader(200, header);
-        response.write(cont);
-        response.end();
-      };
-      if(pathExt === '.js'){
-        send(content, 'Content-Type', 'application/javascript')
-      }else if(pathExt === '.html'){
-        send(content, 'Content-Type', 'text/html');
-      }else if(pathExt === '.css'){
-        send(content, 'Content-Type', 'text/css')
-      }else{
-        send(500);
-      }
+      response.writeHead(400);
+      response.end();
     }
+
   });
+
+  
 
 }
