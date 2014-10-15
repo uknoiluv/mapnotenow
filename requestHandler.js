708fa55d611f61968939b21232b4131981c749db
Adrian Kim <uknoiluv@gmail.com>
2014-10-14 17:09:46 -0700

    10/14_able_to_write_user_in_db

--- requestHandler.js
+++ requestHandler.js
@@ -1,43 +1,64 @@
 
 var fs = require('fs');
 var path = require('path');
+var mongoose = require('mongoose');
 
 var exports = module.exports = {};
 
-exports.handler = function(request, response){
-  var pathName = '.' + request.url;
-  console.log(pathName);
-  if(pathName === './'){
-    pathName = './public/index.html';
-  }
-  fs.exists(pathName, function(exists){
-    if(exists){
-      fs.readFile(pathName, function(err, content){
-        if(err){
-          throw err;
-        }else{
-          var pathExt = path.extname(pathName);
-          var send = function(cont, headKey, headVal){
-            var header = {};
-            header[headKey] = headVal;
-            response.writeHeader(200, header);
-            response.write(cont);
-            response.end();
-          };
-          
-          if(pathExt === '.html'){
-            send(content, 'Content-Type', 'text/html');
-          }else if(pathExt === '.css'){
-            send(content, 'Content-Type', 'text/css')
-          }else{
-            send(content, 'Content-Type', 'text/javascript')
-          }
-        }
-      });
-    }else{
-      response.writeHead(400);
-      response.end();
-    }
-  });
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  username: String,
+  password: String,
+  salt: String
+});
+var User = mongoose.model('User', UserSchema);
 
-}
+
+// exports.handler = function(request, response){
+//   var pathName = '.' + request.url;
+//   console.log(pathName);
+//   if(pathName === './'){
+//     pathName = './public/index.html';
+//   }
+//   fs.exists(pathName, function(exists){
+//     if(exists){
+//       fs.readFile(pathName, function(err, content){
+//         if(err){
+//           throw err;
+//         }else{
+//           var pathExt = path.extname(pathName);
+//           var send = function(cont, headKey, headVal){
+//             var header = {};
+//             header[headKey] = headVal;
+//             response.writeHeader(200, header);
+//             response.write(cont);
+//             response.end();
+//           };
+          
+//           if(pathExt === '.html'){
+//             send(content, 'Content-Type', 'text/html');
+//           }else if(pathExt === '.css'){
+//             send(content, 'Content-Type', 'text/css')
+//           }else{
+//             send(content, 'Content-Type', 'text/javascript')
+//           }
+//         }
+//       });
+//     }else{
+//       response.writeHead(400);
+//       response.end();
+//     }
+//   });
+
+// }
+
+exports.signup = function(user){
+  console.log('user', user);
+  var newUser = new User(user);
+  newUser.save(function(err, user){
+    if(err){
+      console.log('error');
+    }
+    console.log('saved inside DB');
+  });  
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/mapnotenow');
-// var requestHandler = require('./requestHandler.js');
+var requestHandler = require('./requestHandler.js');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
@@ -16,7 +16,7 @@ var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
   res.render('index');
@@ -28,6 +28,7 @@ var server = app.listen(3000, function(){
 
 app.post('/signup', function(req, res){
   console.log('req.body', req.body);
+  requestHandler.signup(req.body);
   // console.log('res', res);
   res.send('hello world!!');
 });
